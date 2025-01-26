45ce7156f2dcd3e33a42622722c7e66bdd728fe9
Imran-2022 <mdimranulhaque.hstu@gmail.com>
2025-01-26 23:54:18 +0600

    take get request from backend1 and than send a response accordingly

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -18,7 +18,24 @@ const newUser = async(req,res)=>{
     return res.status(201).send(profile);
 }
 
+
+const getUserProfile = async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const profile = await Profile.findOne({ userId }).select('bio interests');
+        if (!profile) return res.status(404).send('Profile not found.');
+
+        return res.status(200).send(profile);
+    } catch (err) {
+        console.error('Error fetching profile:', err.message);
+        return res.status(500).send('An error occurred while retrieving the profile.');
+    }
+};
+
+
 router.route('/profile')
     .post(authorize,newUser)
+router.get('/profile/:id', authorize, getUserProfile);
 
 module.exports=router;
